c2fb6bcfab3d2ef1adaab3cd1c9e667c2bceee9c
Keita-Ide <Keita-Ide@github.com>
2021-12-18 16:45:47 +0900

    lint...

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,6 +4,7 @@ from app.calc import add, subtract
 class CaluTests(TestCase):
     def test_add_numbers(self):
         self.assertEqual(add(3, 8), 11)
-    
+
+
     def test_add_numbers(self):
         self.assertEqual(subtract(5, 2),3)
