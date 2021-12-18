f1614df7656d2396ab4e9c7ae1180b1e5c859e9c
Keita-Ide <Keita-Ide@github.com>
2021-12-18 16:53:34 +0900

    modify

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,9 +2,10 @@ from django.test import TestCase
 from app.calc import add, subtract
 
 class CaluTests(TestCase):
+
+
     def test_add_numbers(self):
         self.assertEqual(add(3, 8), 11)
-
 
     def test_add_numbers(self):
         self.assertEqual(subtract(5, 2),3)
