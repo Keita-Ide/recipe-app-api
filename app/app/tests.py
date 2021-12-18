dc5839498cbf73ae3053a184dd21bc61b64a633b
Keita-Ide <Keita-Ide@github.com>
2021-12-18 16:59:31 +0900

    lint.....

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,11 +1,13 @@
 from django.test import TestCase
 from app.calc import add, subtract
 
+
 class CaluTests(TestCase):
 
 
     def test_add_numbers(self):
         self.assertEqual(add(3, 8), 11)
 
+
     def test_add_numbers(self):
-        self.assertEqual(subtract(5, 2),3)
+        self.assertEqual(subtract(5, 2), 3)
